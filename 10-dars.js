61fcbcf8539aa509e37a8eddb01a3f5f2399d692
Zinnur <a.zinnurbek@gmail.com>
2022-01-18 16:30:34 +0500

    commit yozildi

---
+++ 10-dars.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const firstName = "Zinnur";
+const age = 19; 
+const kurs = "4-kursman";
+
+console.log(`Mani ismim ${firstName} va yoshim ${age} hozirda ${kurs}.`);
+
+const a = prompt("Enter your name:");
+console.log(`User's name: ${a}`);
